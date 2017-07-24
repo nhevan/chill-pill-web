e92d9347b382527526e0cb69e490ee15715f8a47
NH Evan <nhevan@gmail.com>
2017-07-25 00:11:11 +0600

    Use Materialize both css and js. Sample nav bar implementation on progress.

---
+++ resources/views/materialize-nav.blade.php
@@ -0,0 +1,65 @@
+{{-- <div class="navbar-header">
+    <!-- Collapsed Hamburger -->
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+
+    <!-- Branding Image -->
+    <a class="navbar-brand" href="{{ url('/') }}">
+        {{ config('app.name', 'Laravel') }}
+    </a>
+</div>
+
+<div class="collapse navbar-collapse" id="app-navbar-collapse">
+    <!-- Left Side Of Navbar -->
+    <ul class="nav navbar-nav">
+        <li><a href="{{ route('viewControls') }}">Test Controls</a></li>
+    </ul>
+
+    <!-- Right Side Of Navbar -->
+    <ul class="nav navbar-nav navbar-right">
+        <!-- Authentication Links -->
+        @if (Auth::guest())
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}">Register</a></li>
+        @else
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        @endif
+    </ul>
+</div> --}}
+<nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">
+        {{-- <img src="/images/chill-pill-logo-1.png" alt="chill pill" style="width:50px;padding: 5px; position: relative; top: 7px;"> --}}
+        Chill Pill
+      </a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        @include('nav.links')
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        @include('nav.links')
+      </ul>
+    </div>
+  </nav>
